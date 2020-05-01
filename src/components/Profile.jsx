cd14d8d3fdd17383e7dae5970275c803a99f1ac1
Santa <alemaskin@im2000.ru>
2020-05-01 20:29:38 +0300

    Test commit. Save module.css

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,21 +1,19 @@
 import React from "react";
+import s from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
         <img src="https://avatars.mds.yandex.net/get-pdb/2497652/72c3a174-9af0-41f6-9f10-e291c329c49b/s1200?webp=false"></img>
       </div>
-      <div>
-        <a>ava + description</a>
-      </div>
-      <div>
+      <div className={s.item}>
         <a>My Posts</a>
       </div>
-      <div>
+      <div className={s.item}>
         <a>post 1</a>
       </div>
-      <div>
+      <div className={s.item}>
         <a>post 2</a>
       </div>
     </div>
